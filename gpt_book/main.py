923b4b1d6ba46c3aa3adb7a162c4f78dcfeec6c0
Sławomir Paszko <folkus@gmail.com>
2024-02-16 22:51:21 +0100

    Reoganized code, added better view and html.

--- gpt_book/main.py
+++ gpt_book/main.py
@@ -22,10 +22,14 @@ def gradio_setup(ai_models: list, text_process: callable) -> gr.Interface:
     # Gradio file input field
     input_file = gr.components.File(label="Input Text File")
 
+    ai_models_str = [
+        f"{model_name} / {token_size}" for model_name, token_size in ai_models
+    ]
+
     # Gradio Combo box for OpenAI model selection
     model = gr.components.Dropdown(
         label="Model",
-        choices=ai_models,
+        choices=ai_models_str,
     )
 
     # Gradio HTML output where two <div's> side by side input and
@@ -43,6 +47,15 @@ def gradio_setup(ai_models: list, text_process: callable) -> gr.Interface:
     return interface
 
 
+def view_html(input_text: str, output_text: str) -> str:
+    """Reads views/view.html file and replaces INPUT as input_text and OUTPUT as output_text"""
+    with open("views/view.html", "r") as file:
+        html = file.read()
+
+    html = html.replace("INPUT", input_text).replace("OUTPUT", output_text)
+    return html
+
+
 def text_process(input_file: str, model: str) -> str:
     """
     Process input file and return output comparison.
@@ -55,9 +68,17 @@ def text_process(input_file: str, model: str) -> str:
     output_text = f"Output from {model} model"
 
     # Return output comparison
-    return f"<div>{input_text}</div><div>{output_text}</div>"
+    return view_html(input_text, output_text)
+
+
+def models_list():
+    """Use Langchain openai to list all available chat models"""
+    from langchain_openai import OpenAI
+
+    openai = OpenAI()
+    return openai.list_models()
 
 
 if __name__ == "__main__":
-    ai_models = ["GPT-3", "GPT-4"]
+    ai_models = [("gpt-3.5-turbo-0125", 16385), ("gpt-4", 8192)]
     gradio_setup(ai_models=ai_models, text_process=text_process).launch()
